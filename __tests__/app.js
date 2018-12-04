f4a472a1588e58b0afef032b75216c35fba90e34
Jon Urry <jon@urry.me>
2018-12-04 22:02:40 +0000

    Further tests for CLI arguments

--- __tests__/app.js
+++ __tests__/app.js
@@ -7,15 +7,30 @@ describe('Command Line Interface (CLI)', () => {
         new CLI();
       }).toThrowError('expecting 2 arguments, was given 0');
     });
+
     test('one arguments given', () => {
       expect(() => {
         new CLI(['']);
       }).toThrowError('expecting 2 arguments, was given 1');
     });
+
     test('three arguments given', () => {
       expect(() => {
         new CLI(['', '', '']);
       }).toThrowError('expecting 2 arguments, was given 3');
+    });
+  });
+  describe('check arguments', () => {
+    test('first argument should be a word', () => {
+      expect(() => {
+        new CLI(['', '']);
+      }).toThrowError('first argument must be a word');
+    });
+
+    test('second argument should be an array', () => {
+      expect(() => {
+        new CLI(['word', '']);
+      }).toThrowError('second argument must be a file name');
     });
   });
 });
--- src/app.js
+++ src/app.js
@@ -2,15 +2,23 @@
 
 const args = process.argv.slice(2);
 
+const checkArguments = args => {
+  if (args.length !== 2) {
+    throw `expecting 2 arguments, was given ${args.length}`;
+  }
+
+  if (args[0].length === 0) {
+    throw 'first argument must be a word';
+  }
+
+  if (args[1].length === 0) {
+    throw 'second argument must be a file name';
+  }
+};
+
 export default class CLI {
   constructor(args = []) {
-    if (args.length !== 2) {
-      throw `expecting 2 arguments, was given ${args.length}`;
-    }
-
-    if (args[0].length === 0) {
-      throw 'first argument must be a word';
-    }
+    checkArguments(args);
   }
 }
 
