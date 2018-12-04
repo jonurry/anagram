9dc92a726233084529c1526a7c03087164691769
Jon Urry <jon@urry.me>
2018-12-04 18:25:52 +0000

    Test for matching anagrams
    Given a word and a dictionary of words,
    determine which of the words in the dictionary
    is an anagram of the original word.
    Make the match case insensitive.

--- __tests__/anagram.js
+++ __tests__/anagram.js
@@ -1,21 +1,27 @@
 import Anagram from '../src/anagram.js';
 
 describe('anagram', () => {
-  describe('it should take two arguments', () => {
-    let anagram;
-    let word = 'SILENT';
-    let dictionary = ['Silent', 'Banana', 'Listen'];
+  let anagram;
+  let word = 'SiLeNt';
+  let dictionary = ['Silent', 'Banana', 'Listen'];
 
-    beforeEach(() => {
-      anagram = new Anagram(word, dictionary);
-    });
+  beforeEach(() => {
+    anagram = new Anagram(word, dictionary);
+  });
 
+  describe('#constructor - should take two arguments', () => {
     test('first parameter should be a word', () => {
       expect(anagram.word).toEqual(word);
     });
 
     test('second parameter should be an array of words (dictionary)', () => {
       expect(anagram.dictionary).toEqual(dictionary);
+    });
+  });
+
+  describe('#getAnagrams - it should return anagrams of the word', () => {
+    test('it should return anagrams of the word', () => {
+      expect(anagram.getAnagrams()).toEqual(['Listen']);
     });
   });
 });
--- src/anagram.js
+++ src/anagram.js
@@ -1,6 +1,28 @@
+const sortWordAlphabetically = word => {
+  return word
+    .split('')
+    .sort((a, b) => {
+      return a.localeCompare(b);
+    })
+    .join('');
+};
+
+const filterMatchingAnagrams = (originalWord, match, dictionary) => {
+  originalWord = originalWord.toLowerCase();
+  match = match.toLowerCase();
+  return dictionary.filter(word => {
+    word = word.toLowerCase();
+    return originalWord != word && sortWordAlphabetically(word) == match;
+  });
+};
+
 export default class Anagram {
   constructor(word, dictionary) {
     this.word = word;
     this.dictionary = dictionary;
+  }
+  getAnagrams() {
+    const alphaWord = sortWordAlphabetically(this.word);
+    return filterMatchingAnagrams(this.word, alphaWord, this.dictionary);
   }
 }
